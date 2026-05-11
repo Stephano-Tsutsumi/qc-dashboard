37c172daa15eb76f753ee07218141ef10cdaeb5f
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2026-05-11 13:39:58 -0700

    Implement login page redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
 
-export default function Home() {
-  redirect('/dashboard')
+export default async function Home() {
+  const supabase = createClient(cookies())
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (user) {
+    redirect('/dashboard')
+  }
+
+  redirect('/auth/login')
 }
